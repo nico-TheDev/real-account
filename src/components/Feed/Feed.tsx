9ebb82b51b46b1e643b32225aac8548b0373ca9b
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-19 19:48:49 +0800

    Trial post component

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import CreatePost from "./CreatePost";
+import Post from "./Post";
 
 export default function Feed() {
     return (
-        <div className="col-span-2 h-full grid grid-cols-2 gap-2 p-4 border-l border-gray-400">
-            <CreatePost />
+        <div className="col-span-2 h-full grid grid-cols-2 gap-6 p-4 border-l border-r border-gray-400 grid-flow-row-dense overflow-auto">
+            <div className="grid gap-4 content-start">
+                <CreatePost />
+                <Post />
+                <Post />
+            </div>
+            <div className="grid gap-4 content-start">
+                <Post />
+                <Post />
+            </div>
         </div>
     );
 }
